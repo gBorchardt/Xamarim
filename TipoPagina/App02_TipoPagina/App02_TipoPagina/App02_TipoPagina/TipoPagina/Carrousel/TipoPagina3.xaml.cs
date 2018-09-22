71a49ff13b8aca601ea018f65267cf918fe2e9de
Gabriel Madeira Borchardt <gabriel.borchardt@hotmail.com>
2018-09-22 12:02:12 -0300

    Xamarin - Tipos de Página

---
+++ TipoPagina/App02_TipoPagina/App02_TipoPagina/App02_TipoPagina/TipoPagina/Carrousel/TipoPagina3.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App02_TipoPagina.TipoPagina.Carrousel
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TipoPagina3 : ContentPage
+	{
+		public TipoPagina3 ()
+		{
+			InitializeComponent ();
+		}
+
+        private void MudarPagina(Object sender, EventArgs args)
+        {
+            //Navigation
+            //App.Current.MainPage = new NavigationPage(new Navigation.Pagina1()) { BackgroundColor = Color.Blue };
+
+            //Tabbed
+            App.Current.MainPage = new Tabbed.Abas();
+        }
+    }
+}
---
+++ TipoPagina/App02_TipoPagina/App02_TipoPagina/App02_TipoPagina/TipoPagina/Navigation/Pagina1.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App02_TipoPagina.TipoPagina.Navigation
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Pagina1 : ContentPage
+	{
+		public Pagina1 ()
+		{
+			InitializeComponent ();
+		}
+
+        private void MudarParaPagina2(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Pagina2());
+        }
+
+        private void MudarParaModal(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Modal());
+        }
+
+        private void MudarParaMaster(object sender, EventArgs args)
+        {
+            App.Current.MainPage = new Master.Master();
+        }
+    }
+}
