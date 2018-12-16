0dd68814de0101a6e9f5578e56cae38bfd862a94
Gabriel Madeira Borchardt <gabriel.borchardt@hotmail.com>
2018-12-15 21:53:53 -0300

    App - Controle Celulas

---
+++ TipoCelula/App07_Cell/App07_Cell/App07_Cell/Menu/Master.xaml.cs
@@ -0,0 +1,39 @@
+﻿using App07_Cell.Pagina;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App07_Cell.Menu
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Master : MasterDetailPage
+	{
+		public Master ()
+		{
+			InitializeComponent ();
+		}
+
+        private void GoTextCellPage(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new TextCellPage());
+            IsPresented = false;
+        }
+
+        private void GoImageCellPage(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new ImageCellPage());
+            IsPresented = false;
+        }
+
+        private void GoEntryCellPage(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new EntryCellPage());
+            IsPresented = false;
+        }
+    }
+}
---
+++ TipoCelula/App07_Cell/App07_Cell/App07_Cell/Pagina/ImageCellPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using App07_Cell.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App07_Cell.Pagina
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ImageCellPage : ContentPage
+	{
+		public ImageCellPage ()
+		{
+			InitializeComponent ();
+
+            var funcionarios = new List<Funcionario>
+            {
+                new Funcionario() { Foto = "perfil.png", Nome = "Gabriel", Cargo = "Desenvolvedor"},
+                new Funcionario() { Foto = "perfil.png", Nome = "Marcos", Cargo = "Analista" },
+                new Funcionario() { Foto = "perfil.png", Nome = "Pedro", Cargo = "Tester" },
+                new Funcionario() { Foto = "perfil.png", Nome = "Paulo Fernando", Cargo = "Suporte" },
+                new Funcionario() { Foto = "perfil.png", Nome = "Kleber", Cargo = "Gerente" }
+            };
+
+            ListaFuncionarios.ItemsSource = funcionarios;
+        }
+	}
+}
