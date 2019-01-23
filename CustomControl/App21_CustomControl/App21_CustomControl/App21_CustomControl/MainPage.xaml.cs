c256191757f49d8c6d308174ca2064e6b2892653
gBorchardt <gabriel.borchardt@hotmail.com>
2019-01-23 15:57:22 -0200

    app CustomControl

---
+++ CustomControl/App21_CustomControl/App21_CustomControl/App21_CustomControl/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App21_CustomControl
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void MyControl_Tapped(object sender, EventArgs e)
+        {
+            DisplayAlert("Evento","Fui Executado", "OK");
+        }
+    }
+}
