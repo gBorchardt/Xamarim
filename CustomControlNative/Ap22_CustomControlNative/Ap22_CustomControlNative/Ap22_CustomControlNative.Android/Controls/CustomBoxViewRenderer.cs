7de072fc84cf0bc62a5d5d4a85e4e1e69f8a2c5e
gBorchardt <gabriel.borchardt@hotmail.com>
2019-01-24 14:28:49 -0200

    App CustomControleNative

---
+++ CustomControlNative/Ap22_CustomControlNative/Ap22_CustomControlNative/Ap22_CustomControlNative.Android/Controls/CustomBoxViewRenderer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Ap22_CustomControlNative.Controls;
+using Ap22_CustomControlNative.Droid.Controls;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly:ExportRenderer(typeof(CustomBoxView),typeof(CustomBoxViewRenderer))]
+namespace Ap22_CustomControlNative.Droid.Controls
+{
+    public class CustomBoxViewRenderer : BoxRenderer
+    {
+        public CustomBoxViewRenderer()
+        {
+            SetWillNotDraw(false);
+        }
+
+        public override void Draw(Canvas canvas)
+        {
+            base.Draw(canvas);
+
+            var control = (CustomBoxView)Element;
+
+            var p = new Paint();
+            p.StrokeWidth = (float)control.Espessura;
+            p.Color = Android.Graphics.Color.Black;
+            p.SetStyle(Paint.Style.Stroke);
+
+            var rect = new Rect(0, 0, 200, 200);
+
+            canvas.DrawRect(rect, p);
+
+            canvas.DrawLine(100, 0, 100, 200, p);
+
+            canvas.DrawLine(0, 100, 200, 100, p);
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            Invalidate();
+        }
+    }
+}
---
+++ CustomControlNative/Ap22_CustomControlNative/Ap22_CustomControlNative/Ap22_CustomControlNative.iOS/Controls/CustomBoxViewRenderer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Ap22_CustomControlNative.Controls;
+using Ap22_CustomControlNative.iOS.Controls;
+using CoreGraphics;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomBoxView), typeof(CustomBoxViewRenderer))]
+namespace Ap22_CustomControlNative.iOS.Controls
+{
+    public class CustomBoxViewRenderer : BoxRenderer
+    {
+        public CustomBoxViewRenderer()
+        {
+            
+        }
+
+        public override void Draw(CGRect rect)
+        {
+            //base.Draw(rect);
+
+            var control = (CustomBoxView)Element;
+
+            using (var context = UIGraphics.GetCurrentContext())
+            {
+                context.SetStrokeColor(new CGColor(0, 0, 0));
+                context.SetLineWidth((float)control.Espessura);
+
+                var rectPath = new CGRect(0, 0, 200, 200);
+
+                context.AddRect(rectPath);
+                context.DrawPath(CGPathDrawingMode.Stroke);
+            }
+
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            //base.OnElementPropertyChanged(sender, e);
+
+            if (e.PropertyName == CustomBoxView.EspessuraProperty.PropertyName)
+                SetNeedsDisplay();
+        }
+    }
+}
