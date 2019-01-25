cbd15930da698ed78fcb89c3c2faac203873aac8
gBorchardt <gabriel.borchardt@hotmail.com>
2019-01-25 16:24:23 -0200

    App exemplos para detectar SO

---
+++ DetectarSO/App23_DetectarSO/App23_DetectarSO/App23_DetectarSO/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App23_DetectarSO
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            if (Device.Idiom == TargetIdiom.Tablet)
+                SLContainer.BackgroundColor = Color.Aqua;
+
+            //Opção 1
+            if (Device.OS == TargetPlatform.iOS)
+                SLContainer.Margin = new Thickness(0, 10, 0, 0);
+
+            //Opção 2
+            var margin = new Thickness();
+            
+            Device.OnPlatform(iOS: () => {
+                margin = new Thickness(0, 10, 0, 0);
+            }, Android: () => {
+            }, WinPhone: () => { });
+
+            SLContainer.Margin = margin;
+        }
+    }
+}
