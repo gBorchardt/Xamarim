05525b4cea95c0f3179a5788a0d7dc5c7ea89214
gBorchardt <gabriel.borchardt@hotmail.com>
2019-02-05 17:50:53 -0200

    App Using Plugins

---
+++ UsingPlugins/App27_UsingPlugins/App27_UsingPlugins/App27_UsingPlugins/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Plugin.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App27_UsingPlugins
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnFoto.Clicked += TirarFoto;
+
+        }
+
+        private async void TirarFoto(object origem, EventArgs args)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "Sample",
+                Name = "test.jpg"
+            });
+
+            if (file == null)
+                return;
+
+            await DisplayAlert("File Location", file.Path, "OK");
+
+            imgFoto.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                return stream;
+            });
+        }
+    }
+}
