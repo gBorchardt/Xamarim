b972541f9321454970462a4f7a744aaad2e59fdb
gBorchardt <gabriel.borchardt@hotmail.com>
2019-02-06 17:45:42 -0200

    App StorageFile, usando plugin PCLStorage

---
+++ StorageFile/App28_StorageFile/App28_StorageFile/App28_StorageFile/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App28_StorageFile
+{
+    public partial class MainPage : ContentPage
+    {
+        const string filename = "file.txt";
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnEscrever.Clicked += async (sender, args) =>
+            {
+                Util.StorageManager.SalvarConteudo(filename, txtConteudo.Text);
+                txtConteudo.Text = string.Empty;
+            };
+
+            btnLer.Clicked += async (sender, args) =>
+            {
+                lblLeitura.Text = await Util.StorageManager.LerConteudo(filename);
+            };
+        }
+    }
+}
---
+++ StorageFile/App28_StorageFile/App28_StorageFile/App28_StorageFile/Util/StorageManager.cs
@@ -0,0 +1,28 @@
+﻿using PCLStorage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App28_StorageFile.Util
+{
+    //https://github.com/dsplaisted/PCLStorage
+    public class StorageManager
+    {
+        public async static void SalvarConteudo(string fileName, string conteudo)
+        {
+            IFolder rootFolder = FileSystem.Current.LocalStorage;
+            IFolder folder = await rootFolder.CreateFolderAsync("arquivos",CreationCollisionOption.OpenIfExists);
+            IFile file = await folder.CreateFileAsync(fileName, CreationCollisionOption.ReplaceExisting);
+            await file.WriteAllTextAsync(conteudo);
+        }
+
+        public async static Task<string> LerConteudo(string fileName)
+        {
+            IFolder rootFolder = FileSystem.Current.LocalStorage;
+            IFolder folder = await rootFolder.CreateFolderAsync("arquivos",CreationCollisionOption.OpenIfExists);
+            IFile file = await folder.GetFileAsync(fileName);
+            return await file.ReadAllTextAsync();
+        }
+    }
+}
