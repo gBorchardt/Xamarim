10f06b5f8db14b0c658f325866bdf5fb0d45b79b
gBorchardt <gabriel.borchardt@hotmail.com>
2019-02-08 19:52:34 -0200

    App Usando Fontes Personalizadas

---
+++ FontesPersonalizadas/App31_FontesPersonalizadas/App31_FontesPersonalizadas/App31_FontesPersonalizadas/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App31_FontesPersonalizadas
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //Buscar Fontes: www.urbanfonts.com
+            //Extensão .ttf
+            //Android - Colar na pasta Assets
+            //IOS - Colar na pasta Resource E Criar Font no Info.plist
+
+            lblInicio.FontFamily = Device.OnPlatform(
+                                        "Lily of  the Valley_Personal_Use",
+                                        "LilyoftheValley.ttf#Lily of  the Valley_Personal_Use",
+                                        "Assets/Fonts/LilyoftheValley.ttf#Lily of  the Valley_Personal_Use"
+                                    );
+        }
+    }
+}
